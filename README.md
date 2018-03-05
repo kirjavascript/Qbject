93dc5d4a884b17412e574ee42b54716fded438d5
kirjavascript <snkenjoi@gmail.com>
2018-03-05 20:36:05 +0000

    2.0.1

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
-Optional object chaining with ES6 Proxys
+Optional object chaining & creation with ES6 Proxys
 
-You could use [this](https://lodash.com/docs/4.17.4#get) or [this](https://www.npmjs.com/package/babel-plugin-transform-optional-chaining) or [this](https://github.com/tc39/proposal-optional-chaining) instead
+You could use [this](https://lodash.com/docs/4.17.4#get) or [this](https://www.npmjs.com/package/babel-plugin-transform-optional-chaining) or [this](https://github.com/tc39/proposal-optional-chaining) instead if you just need optional chaining.
+
+On setting a value, creates objects when they don't already exist.
+
+Exports a global 'Qbject' when dumped in your page as a script tag.
 
 Install
 ---
@@ -16,25 +20,26 @@ Params
     default value (undefined)
     unwrap key ('_')
 
-Exports a global 'Qbject' when dumped in your page as a script tag
-
 Examples
 ---
 
 ```javascript
-import Qbject from 'qbject';
-
 const qbj1 = new Qbject();
+
+console.log(qbj1.bar.bar.baz._); // 'undefined'
+
+qbj1.arbitrary.deep.mutation = true;
+
+console.log(qbj1._); // {"arbitrary":{"deep":{"mutation":true}}}
 
 qbj1.foo = 'data';
 
-console.log(qbj1.foo._); // prints 'data'
-console.log(qbj1.bar.bar.baz._); // prints 'undefined'
+console.log(qbj1.foo._); // 'data'
 
 const qbj2 = new Qbject({foo: 'test'}, 'something');
 
-console.log(qbj2._); // prints { foo: 'test' }
-console.log(qbj2.foo._); // prints 'test'
-console.log(qbj2.bar._); // prints 'something'
-console.log(qbj2.bar.bar._); // prints 'something'
+console.log(qbj2._); // { foo: 'test' }
+console.log(qbj2.foo._); // 'test'
+console.log(qbj2.bar._); // 'something'
+console.log(qbj2.bar.bar._); // 'something'
 ```
--- index.js
+++ index.js
@@ -10,12 +10,13 @@
 
     return function(obj = {}, _default = void 0, valueKey = '_') {
         return new Proxy(obj, {
-            get(target, key) {
-                if (key == valueKey) {
+            get(target, rootkey) {
+                if (rootkey == valueKey) {
                     return target;
                 }
                 else {
                     let clone = Object.assign({}, obj);
+                    const path = [];
                     const proxy = new Proxy(clone, {
                         get(target, key) {
                             if (key == valueKey) {
@@ -23,12 +24,32 @@
                             }
                             else {
                                 clone = clone ? clone[key] || _default : _default;
+                                path.push(key)
                                 return proxy;
                             }
-                        }
+                        },
+                        set(_, key, value) {
+                            if (typeof obj[rootkey] != 'object') {
+                                obj[rootkey] = {};
+                            }
+                            path.shift(); // we already did the root key
+                            let target = obj[rootkey];
+                            path.push(key); // add the leaf
+                            path.forEach((cd, i) => {
+                                if (typeof target[cd] != 'object') {
+                                    target[cd] = {};
+                                }
+                                if (i == path.length - 1) {
+                                    target[cd] = value;
+                                }
+                                else {
+                                    target = target[cd];
+                                }
+                            });
+                        },
                     });
 
-                    return proxy[key];
+                    return proxy[rootkey];
                 }
             }
         });
